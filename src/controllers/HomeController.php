0fe026f4c8b046f0e1d7dc50e00ae1707bb7daaa
2011 Alexandre <2011brunofernandes@gmail.com>
2022-07-14 16:51:34 -0300

    exclusao dearquivos desnecessarios

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -6,15 +6,9 @@ use \core\Controller;
 class HomeController extends Controller {
 
     public function index() {
-        $this->render('home', ['nome' => 'Bonieky']);
+        $this->render('home');
     }
 
-    public function sobre() {
-        $this->render('sobre');
-    }
 
-    public function sobreP($args) {
-        print_r($args);
-    }
 
 }
